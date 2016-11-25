464dd227dbb502d2c00aad1c8e72610b56ef2aa0
Dan Nguyen <dansonguyen@gmail.com>
2016-11-24 16:24:48 -0800

    first

---
+++ scripts/convert-atf.py
@@ -0,0 +1,33 @@
+"""
+Expects a single filename argument
+converts tab delimitation to CSV
+cuts out second-line of inkjunk
+
+python scripts/convert-atf.py \
+   data/raw/atf_firearm_dealers_2016_08.txt \
+   > data/wrangled/atf_firearm_dealers_2016_08.csv
+"""
+
+import csv
+from datetime import date
+from re import search
+from sys import argv, stdout
+
+if __name__ == '__main__':
+    srcpath = argv[1]
+    wcsv = csv.writer(stdout)
+    with open(srcpath, 'r') as rf:
+        rcsv = csv.reader(rf, delimiter='\t')
+        wcsv.writerow(next(rcsv)) # write header
+        next(rcsv) # skip second line of non-data
+        for row in rcsv:
+            values = [v.strip() for v in row]
+            if values[0]: # skip blank lines
+                # last field is the `Expire Date` field
+                # which we want to convert to YYYY-MM-DD format
+                dt = search(r'(\d{1,2})/(\d{1,2})/(\d{4})', values[-1])
+                if dt:
+                    month, day, year = [int(d) for d in dt.groups()]
+                    values[-1] = date(year, month, day).isoformat()
+                wcsv.writerow(values)
+
---
+++ todo.md
@@ -0,0 +1,55 @@
+- Plot biggest earthquakes
+- Plot animated map of earthquakes
+
+
+- Plot density of SF crimes
+- Plot density of earthquakes
+
+
+- Add state shapefiles and rail data
+- Add California rail data
+- Add SF Census tracts, shade by diversity
+
+
+Joins
+
+- Join Congressional districts with election data
+- Join state and election result data
+- Ohio county level data, 2008, 2012, 2016
+- Ohio county level election data and demographic data
+- Starbucks by zips
+- Gun stores vs Starbucks by zips
+- All zips where gun stores out number starbucks
+- All zips where gun stores out number starbucks, outline the state election results
+- Chiuaua by la zip code
+- Chiuaua by number of 250,000+ income homes
+
+
+
+Spatial operations
+
+- Make earthquake dots
+- Earthquakes within state boundaries
+- Census tracts intersected by Caltrain
+- Census tracts with latitude/longitude
+- Gun stores vs Starbucks by Congressional District
+
+
+# Projects
+
+- Which parking lots have the most thefts?
+
+
+
+
+
+
+
+
+- Plot earthquakes by state with PostGIS
+
+
+
+
+# Mapping
+
